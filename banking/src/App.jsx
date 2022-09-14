a55992a29baa3d6c5ffcb090ba584f942f7fdf32
sivasaikumar <sivasaikumar.pinjala@gmail.com>
2022-09-14 23:37:23 +0530

    updated app

--- banking/src/App.jsx
+++ banking/src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-
+import styles from './style'
 const App = () => {
   return (
-    <div>App</div>
+    <div className='bg-primary w-full overflow-hidden'>
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+            navbar
+          </div>
+        </div>
+
+        <div className={`bg-primary ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            Hero
+          </div>
+        </div>
+
+        <div className={`bg-primary ${styles.flexStart} ${styles.paddingX}`}>
+          <div className={`${styles.boxWidth}`}>
+            Stats
+            Business
+            cardDeal
+            
+          </div>
+        </div>
+    </div>
   )
 }
 
